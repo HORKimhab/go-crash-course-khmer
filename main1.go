45e90f28fbe7c7f4ff5437a84556ebefeb7d9c9b
Anoskh <kimhab.zeecode@gmail.com>
2022-05-16 21:24:25 +0700

    First push golang course

---
+++ main1.go
@@ -0,0 +1,18 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+// fmt to running go project
+import "fmt"
+
+/*
+	main() is from package main
+	learning Golang with HKimhab
+*/
+func main() {
+	fmt.Println("Hello Wolrd.")
+	fmt.Println("Hello Universe.")
+	fmt.Println("Hello Universe by HKimhab in Golang.")
+	// Single Line comment 
+	fmt.Println("Hello Crush, nice to see you.")
+}
